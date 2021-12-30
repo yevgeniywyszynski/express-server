3883cfe901a2550a28f5612e92b0189bce98d914
YevgeniyWyszynski <ywyszynski@gmail.com>
2021-12-30 12:17:40 +0100

    Add handlebars

--- server.js
+++ server.js
@@ -1,41 +1,49 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
-app.use((req, res, next) => {
-    res.show = (name) => {
-      res.sendFile(path.join(__dirname, `/views/${name}`));
-    };
-    next();
-  });
+/*app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+*/
 
-  app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
-  app.get('/', (req, res) => {
-      res.show('index.html');
-  })
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
-  app.get('/home', (req, res) => {
-      res.show('index.html');
-  })
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
+});
 
-  app.get('/about', (req, res) => {
-      res.show('about.html');
-  })
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
-  app.use('/user', (req, res) => {
-      res.show('login.html')
-  })
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
 
-  app.use((req, res) => {
-    res.status(404).show('404.html')
-  })
+app.get('/info', (req, res) => {
+  res.render('info');
+});
 
+app.get('/history', (req, res) => {
+  res.render('history');
+});
 
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
+})
 
-
-
-  app.listen(8000, () => {
-    console.log('Server is running on port: 8000');
-  });
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
