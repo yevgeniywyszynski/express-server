7a6a50ea130ab8dc4560aab409b1d6dad6774d78
YevgeniyWyszynski <ywyszynski@gmail.com>
2022-01-04 12:17:44 +0100

    Add new method post and add new input

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const hbs = require('express-handlebars');
 const app = express();
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
+app.use(express.urlencoded({ extended: false }));
 
 /*app.use((req, res, next) => {
-  res.show = (name) => {
+  res.show = (name) => {ello
     res.sendFile(path.join(__dirname, `/views/${name}`));
   };
   next();
@@ -24,6 +25,12 @@ app.get('/hello/:name', (req, res) => {
   res.render('hello', { name: req.params.name });
 });
 
+app.get('/sum/:a/:b', (req, res) => {
+  let numberA = parseInt(req.params.a);
+  let numberB = parseInt(req.params.b);
+  res.render('hello', {name: numberA+numberB})
+})
+
 app.get('/about', (req, res) => {
   res.render('about');
 });
@@ -39,6 +46,18 @@ app.get('/info', (req, res) => {
 app.get('/history', (req, res) => {
   res.render('history');
 });
+//''==false
+app.post('/contact/send-message', (req, res) => {
+
+  const { author, sender, title, message, img } = req.body;
+  
+  if(author && sender && title && message && img) {
+    res.render('contact', { isSent: true});
+  }
+  else {
+    res.render('contact', { isError: true})
+  }
+});
 
 app.use((req, res) => {
   res.status(404).send('404 not found...');
@@ -47,3 +66,11 @@ app.use((req, res) => {
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
+
+//przegladatka <--- server
+
+//przegladarka ----> server
+
+//localhost:8000/sum/4/5 => 9
+//localhost:8000/sum/10/5 => 15
+//localhost:8000/sum/2/8 => 10
